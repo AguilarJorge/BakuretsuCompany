4f9a3dd0ba82d06647bee9a9421bf58ff2c4ad37
Jorge Aguilar <jorgeluis_942009@hotmail.com>
2020-10-08 19:15:33 -0500

    Footer y manejo de boton de filtros

--- js/todos.js
+++ js/todos.js
@@ -1,4 +1,21 @@
 $(function(){
+    //Mostrar/ocultar boton de filtros en base al viewport
+    if ($('.bakuretsu_catalogo').length) {
+        const seccion = $('.bakuretsu_catalogo');
+        var windowHeight = $(window).height();
+        var height = seccion[0].getBoundingClientRect().height;
+        var y = seccion[0].getBoundingClientRect().y;
+        var calculo = windowHeight + Math.abs(y);
+        var launchF = seccion.find('.showFiltrosResponsive');
+        height >= calculo ? launchF.removeClass('oculto') : launchF.addClass('oculto');
+        $(window).scroll(function(){
+            windowHeight = $(window).height();
+            height = seccion[0].getBoundingClientRect().height;
+            y = seccion[0].getBoundingClientRect().y;
+            calculo = windowHeight + Math.abs(y);
+            height >= calculo ? launchF.removeClass('oculto') : launchF.addClass('oculto');
+        })
+    }
     //Filtros
     $('.bakuretsu_filtros').on('click', '.grupo .headerGrupo', function(){
         $(this).parent().toggleClass('show');
