67cddd0c9c76e47a56781644590d6fc19b6f6846
Jorge Aguilar <jorgeluis_942009@hotmail.com>
2021-03-02 19:21:16 -0600

    + Vistas

--- js/compartido.js
+++ js/compartido.js
@@ -1,4 +1,119 @@
 $(function(){
+  //Alerta tipo modal
+  const alertaModal = function (config) {
+    config = jQuery.extend({
+      acciones: {
+        aprov: 'Aceptar',
+        cancel: 'Cancelar'
+      },
+      closable: false,
+      closeOnActions: true,
+      redondeado: true,
+      encabezado: 'Encabezado de la modal',
+      mensaje: 'Mensaje de la modal...',
+      ocultar: false,
+      tipo: 'warning',
+      // onAprov, onCancel //Callbacks
+    }, config);
+    var acciones = '';
+    if (config.acciones) {
+      $.each(config.acciones, function (k, v) {
+        acciones += '<div class="accion ' + k + '">' + v + '</div>';
+      })
+    } else {
+      config.autoclose = '3000';
+    }
+    if (config.ocultar) {
+      $(document).off('click', '.bakuretsu_alertaModal .accion');
+      $('.bakuretsu_alertaModal').fadeOut(function () { $(this).remove() });
+      return false;
+    }
+    var svgIcon;
+    switch (config.tipo) {
+      case 'error':
+        svgIcon = `<svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32.001" cy="32" r="30" transform="rotate(-45 32.001 32)"></circle><path d="M42.999 21.001l-22 22m22 0L21 21"></path></svg>`;
+        break;
+      case 'exito':
+        svgIcon = `<svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32" cy="32" r="30" transform="rotate(-45 32 32)"></circle><path d="M20.998 32.015l8.992 8.992 16.011-16.011"></path></svg>`;
+        break;
+      case 'warning':
+        svgIcon = `<svg class="icono" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><circle cx="32" cy="32" r="30"></circle><path d="M32 15v24"></path><circle cx="32" cy="47" r="2"></circle></svg>`;
+        break;
+    }
+    if ($('.bakuretsu_alertaModal').length == 0) {
+      $('body').append(`
+      <div class="bakuretsu_alertaModal">
+        <div class="contenedor ${config.tipo} ${config.redondeado && 'rounded'}">
+          ${svgIcon || ''}
+          <p class="encabezado">${config.encabezado}</p>
+          <p class="mensaje">${config.mensaje}</p>
+          <div class="acciones">${acciones}</div>
+        </div>
+      </div>
+    `);
+      if (config.closable) $('.bakuretsu_alertaModal').prepend('<div class="closable"></div>');
+      $('.bakuretsu_alertaModal').fadeIn().css('display', 'flex');
+      $(document).on('click', '.bakuretsu_alertaModal .accion', function (e) {
+        if ($(this).hasClass('aprov') && config.onAprov) {
+          config.onAprov(e);
+        } else if ($(this).hasClass('cancel') && config.onCancel) {
+          config.onCancel(e);
+        }
+        if (config.closeOnActions) {
+          $(document).off('click', '.bakuretsu_alertaModal .accion');
+          $('.bakuretsu_alertaModal').fadeOut(function () { $(this).remove() });
+        }
+      })
+      if (config.hasOwnProperty('autoclose')) {
+        setTimeout(function () {
+          $(document).off('click', '.bakuretsu_alertaModal .accion');
+          $('.bakuretsu_alertaModal').fadeOut(function () { $(this).remove() });
+        }, config.autoclose)
+      }
+    } else {
+      $(document).off('click', '.bakuretsu_alertaModal .accion');
+    }
+    $(document).on('click', '.bakuretsu_alertaModal .closable', function () {
+      $(document).off('click', '.bakuretsu_alertaModal .accion');
+      $('.bakuretsu_alertaModal').fadeOut(function () { $(this).remove() });
+    })
+  }
+  const responsivImg = function (img, convFac) {
+    var a = img.width() / convFac;
+    img.css('height', a);
+  }
+  const autoSlider = function (slider, totalSlides, indicadores, avanzar = true) {
+    var posicion = slider.find('.bakuretsu_slide.activo').index();
+    if (avanzar) {
+      posicion == (totalSlides - 1) ? posicion = 0 : posicion++;
+    } else {
+      posicion == 0 ? posicion = (totalSlides - 1) : posicion--;
+    }
+    slider.find('.bakuretsu_slide.activo').removeClass('activo');
+    slider.find('.bakuretsu_slide').eq(posicion).addClass('activo');
+    if (indicadores) {
+      slider.find('.bakuretsu_dot.activo').removeClass('activo');
+      slider.find('.bakuretsu_dot').eq(posicion).addClass('activo');
+    }
+  }
+
+  //Modal para confirmar la edad
+  if ($('.bakuretsu_censored').length) {
+    if (localStorage.getItem('edadStatus') !== 'uncensored') {
+      alertaModal({
+        encabezado: 'Contenido gráfico',
+        mensaje: 'Este sitio incluye contenido gráfico que algunas personas pueden considerar ofensivo o molesto. Es necesario que confirmes que eres mayor de edad para acceder a dicho contenido.',
+        acciones: { aprov: 'Si, tengo más de 18 años', cancel: 'Cancelar' },
+        onAprov: function(){
+          localStorage.setItem('edadStatus', 'uncensored');
+          $('.bakuretsu_censored').each(function (i, censored) { $(censored).removeClass('bakuretsu_censored') });
+        }
+      });
+    } else {
+      $('.bakuretsu_censored').each(function (i, censored) { $(censored).removeClass('bakuretsu_censored') });
+    }
+  }
+
   //Checar storage para ver si activo el dark theme
   if (localStorage.getItem('theme') === 'dark') {
     $('body').addClass('darken');
@@ -143,5 +258,77 @@ $(function(){
     $(this).parent().fadeOut(function(){
       $(this).remove();
     });
+  })
+  //Imagen responsiva
+  if ($('.bakuretsu_responsiv').length) {
+    $('.bakuretsu_responsiv').each(function (i, img) {
+      var fc = parseInt($(img).css('max-width')) / $(img).height();
+      responsivImg($(img), fc);
+      $(window).resize(function () { responsivImg($(img), fc) });
+    })
+  }
+  //Slider
+  if ($('.bakuretsu_slider').length) {
+    $('.bakuretsu_slider').each(function (i, slider) {
+      var { sliderAutomatico, sliderDuracion, sliderControls } = $(slider).data();
+      var slide = $(slider).find('.bakuretsu_slide');
+      var totales = slide.length;
+      if (totales > 0) slide.first().addClass('activo');
+      if (totales > 1) {
+        if (sliderControls) {
+          var wrapperDots = $(slider).find('.bakuretsu_dots');
+          $(slider).append('<div class="manualControls"><div class="bakuretsu_control control circular at"></div><div class="bakuretsu_control control circular av"></div></div>');
+          if (wrapperDots.length) {
+            var dots = wrapperDots;
+          } else {
+            $(slider).append('<div class="bakuretsu_dots"></div>');
+            var dots = $(slider).children('.bakuretsu_dots');
+          }
+          for (var i = 0; i < totales; i++) {
+            dots.append('<div class="bakuretsu_dot"></div>');
+          }
+          dots.children('.bakuretsu_dot').first().addClass('activo');
+          $(slider).on('click', '.manualControls .bakuretsu_control', function () {
+            if ($(this).hasClass('at')) {
+              autoSlider($(slider), totales, sliderControls, false)
+            } else if ($(this).hasClass('av')) {
+              autoSlider($(slider), totales, sliderControls)
+            }
+            if (automatico) {
+              clearInterval(automatico);
+              automatico = setInterval(function () {
+                autoSlider($(slider), totales, sliderControls)
+              }, sliderDuracion || 5000);
+            }
+          })
+          $(slider).on('click', '.bakuretsu_dots .bakuretsu_dot', function () {
+            $(this).siblings().removeClass('activo');
+            $(this).addClass('activo');
+            $(slider).find('.bakuretsu_slide.activo').removeClass('activo');
+            $(slider).find('.bakuretsu_slide').eq($(this).index()).addClass('activo');
+            if (automatico) {
+              clearInterval(automatico);
+              automatico = setInterval(function () {
+                autoSlider($(slider), totales, sliderControls)
+              }, sliderDuracion || 5000);
+            }
+          })
+        }
+        if (sliderAutomatico) {
+          var automatico = setInterval(function () {
+            autoSlider($(slider), totales, sliderControls)
+          }, sliderDuracion || 5000);
+        }
+      } else {
+        $(slide).css('transition', 'none')
+      }
+    })
+  }
+  //Drops Slidables
+  $('.bakuretsu_slidable .slidableTitle').click(function(){
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().toggleClass('active');
+    $(this).parent().siblings().find('.slidableContent:visible').slideUp('fast');
+    $(this).siblings('.slidableContent').slideToggle('fast');
   })
 })
